e7a4e04ef65f72ba2f83a90f963ea34c550a90af
Mario2 <margal15@ucm.es>
2024-10-04 12:20:13 +0200

    Modificado analyze.py para que pille la info de los comentarios, falta pillar los comentarios como nos pasa con los posts y refinarlo para que se vea mejor

--- analyze.py
+++ analyze.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import csv
+import requests
 from pathlib import Path
 
 f = Path("cache") / "index.html"
 t = f.read_text()
+p = "https://old.reddit.com/r/spain/comments/"
 
 soup = BeautifulSoup(t, 'html.parser')
 #print(soup.title)
@@ -13,9 +15,11 @@ soup = BeautifulSoup(t, 'html.parser')
 #print(soup.prettify())
 
 datos_post = []
+datos_comentario = []
 
 for i in soup.findAll("div", {"class": "thing"}):
     id_post = i["data-fullname"]
+    id_pag = p + id_post[3:]
     titulo = (i.find("a", {"class": "title"})).text 
     aux = i.find("a", {"class": "author"})
     if aux:
@@ -23,7 +27,7 @@ for i in soup.findAll("div", {"class": "thing"}):
     else: 
         autor = "Autor desconocido"
     fecha = i.find("time")["title"]
-    descripcion = (i.find("div", {"class": "expando"}))
+    descripcion = (i.find("div", {"class": "md"}))
     datos_post.append({
         'id_post' : id_post,
         'titulo': titulo,
@@ -31,3 +35,23 @@ for i in soup.findAll("div", {"class": "thing"}):
         'fecha': fecha,
         'descripcion': descripcion
         })
+r = requests.get(id_pag)
+soup2 = BeautifulSoup(r.content, 'html.parser')
+
+for i in soup.findAll("div", {"class": "thing"}):
+    id_post = i["data-fullname"]
+    titulo_post = (i.find("a", {"class": "title"})).text
+    autor_post = i.find("a", {"class": "author"}).text
+    fecha_comment = i.find("time")["title"]
+    comentario = i.find("div", {"class": "md"})
+    datos_comentario.append({
+        'comentario' : comentario,
+        'fecha' : fecha_comment,
+        'post al que responde': titulo_post,
+        'autor': autor_post
+        })
+
+print(id_pag)
+print(datos_post)
+print("-------------------------------------------")
+print(datos_comentario)
