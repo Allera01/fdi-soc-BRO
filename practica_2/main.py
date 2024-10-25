e92808f9b0739edc8c2e38e73bc5e1dfc48aa133
Mario <mcarri17@ucm.es>
2024-10-25 14:57:42 +0200

    Esqueleto de creación de gráfico para la polaridad

--- practica_2/main.py
+++ practica_2/main.py
@@ -5,6 +5,7 @@ import re
 from nltk.tokenize import word_tokenize
 from textblob import TextBlob
 import string
+import matplotlib.pyplot as plt
 
 tknzr = nltk.tokenize.TweetTokenizer()
 
@@ -29,10 +30,42 @@ df = pd.read_csv("cyberpunk.csv")
 #df["tokens"] = df["text"].apply(preprocess)
 df['tokens'] = df['text'].apply(remove_punctuation)
 
-all_tokens = [token for tokens_list in df['tokens'] for token in tokens_list]
+""" all_tokens = [token for tokens_list in df['tokens'] for token in tokens_list]
 token_counts = pd.Series(all_tokens).value_counts()
 
 with open('token_counts.txt', 'w') as f:
     f.write(token_counts.to_string())
 
-print(token_counts)
+print(token_counts) """
+
+
+
+df['polarity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+# Filtrar la frecuencia de las palabras "good" y "bad"
+df['good_count'] = df['tokens'].apply(lambda tokens: tokens.count('good'))
+df['bad_count'] = df['tokens'].apply(lambda tokens: tokens.count('bad'))
+
+# Filtrar solo las filas con presencia de "good" o "bad"
+df_filtered = df[(df['good_count'] > 0) | (df['bad_count'] > 0)]
+
+# Crear gráfico de polaridad
+plt.figure(figsize=(10, 6))
+
+# Graficar la frecuencia de "good" en relación con la polaridad
+plt.scatter(df_filtered[df_filtered['good_count'] > 0]['polarity'],
+            df_filtered[df_filtered['good_count'] > 0]['good_count'],
+            color='green', label='Good')
+
+# Graficar la frecuencia de "bad" en relación con la polaridad
+plt.scatter(df_filtered[df_filtered['bad_count'] > 0]['polarity'],
+            df_filtered[df_filtered['bad_count'] > 0]['bad_count'],
+            color='red', label='Bad')
+
+# Personalización del gráfico
+plt.xlabel('Polaridad')
+plt.ylabel('Frecuencia')
+plt.title('Relación de Polaridad con las Palabras "Good" y "Bad"')
+plt.legend()
+plt.grid(True)
+plt.savefig("grafico_polaridad.png")
