50340831e447ae32677cd66e1788095104129427
Allera01 <alvaro@DESKTOP-IA7EM4F>
2024-10-26 20:40:59 +0200

    He generalizado las funciones para graficos de 1 y 2 palabras y añadido algunas comparaciones interesantes

--- practica_2/main.py
+++ practica_2/main.py
@@ -28,11 +28,76 @@ def remove_punctuation(text):
     tokens = [token.lower() for token in tokens if token not in string.punctuation]
     return tokens
 
+def grafico_polaridad_2p(palabra1, palabra2, datos):
+    df = datos
+    df['polarity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+    # Calcular la longitud del tweet
+    df['tweet_length'] = df['text'].apply(len)
+
+    # Filtrar los tweets que contienen las palabras 
+    df['contains_palabra1'] = df['tokens'].apply(lambda tokens: palabra1 in tokens)
+    df['contains_palabra2'] = df['tokens'].apply(lambda tokens: palabra2 in tokens)
+
+    # Filtrar solo las filas con las palabras
+    df_palabra1_palabra2 = df[(df['contains_palabra1']) | (df['contains_palabra2'])]
+
+    # Crear gráfico de polaridad vs longitud del tweet, diferenciando las palabras
+    plt.figure(figsize=(10, 6))
+
+    # Graficar tweets con la palabra1
+    plt.scatter(df_palabra1_palabra2[df_palabra1_palabra2['contains_palabra1']]['tweet_length'],
+                df_palabra1_palabra2[df_palabra1_palabra2['contains_palabra1']]['polarity'],
+                color='blue', label=palabra1, alpha=0.5)
+
+    # Graficar tweets con la palabra2
+    plt.scatter(df_palabra1_palabra2[df_palabra1_palabra2['contains_palabra2']]['tweet_length'],
+                df_palabra1_palabra2[df_palabra1_palabra2['contains_palabra2']]['polarity'],
+                color='red', label=palabra2, alpha=0.5)
+
+    # Personalización del gráfico
+    plt.xlabel('Longitud del Tweet')
+    plt.ylabel('Polaridad')
+    plt.title('Relación entre Polaridad y Longitud del Tweet para ' + palabra1 + ' y ' + palabra2)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("grafico_polaridad_" + palabra1 +"_" + palabra2 +".png")
+    
+def grafico_polaridad_1p(palabra, datos):
+    df = datos
+    df['polarity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+    # Calcular la longitud del tweet
+    df['tweet_length'] = df['text'].apply(len)
+
+    # Filtrar los tweets que contienen la palabra
+    df['contains_palabra'] = df['tokens'].apply(lambda tokens: palabra in tokens)
+
+    # Filtrar solo las filas con la palabra
+    df_palabra = df[(df['contains_palabra'])]
+
+    # Crear gráfico de polaridad vs longitud del tweet
+    plt.figure(figsize=(10, 6))
+
+    # Graficar tweets con palabra
+    plt.scatter(df_palabra[df_palabra['contains_palabra']]['tweet_length'],
+                df_palabra[df_palabra['contains_palabra']]['polarity'],
+                color='green', label=palabra, alpha=0.5)
+
+    # Personalización del gráfico
+    plt.xlabel('Longitud del Tweet')
+    plt.ylabel('Polaridad')
+    plt.title('Relación entre Polaridad y Longitud del Tweet para '+palabra)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("grafico_polaridad_" + palabra +".png")
+    
 df = pd.read_csv("cyberpunk.csv")
 #df["tokens"] = df["text"].apply(preprocess)
 df['tokens'] = df['text'].apply(remove_punctuation)
 
-#Añadir a all_tokens todos los tokens sin repetición
+#descomentar para tener la lista de palabras junto a su numero de apariciones
+"""#Añadir a all_tokens todos los tokens sin repetición
 all_tokens = [token for tokens_list in df['tokens'] for token in tokens_list]
 #Contar la cantidad de veces que aparece cada token
 token_counts = pd.Series(all_tokens).value_counts()
@@ -40,40 +105,12 @@ token_counts = pd.Series(all_tokens).value_counts()
 with open('token_counts.txt', 'w') as f:
     f.write(token_counts.to_string())
 """
-print(token_counts) """
+#print(token_counts) 
 
-
-
-df['polarity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
-
-# Calcular la longitud del tweet
-df['tweet_length'] = df['text'].apply(len)
-
-# Filtrar los tweets que contienen las palabras "good" o "bad"
-df['contains_good'] = df['tokens'].apply(lambda tokens: 'cyberpunk' in tokens)
-#df['contains_bad'] = df['tokens'].apply(lambda tokens: 'character' in tokens)
-
-# Filtrar solo las filas con "good" o "bad"
-#df_good_bad = df[(df['contains_good']) | (df['contains_bad'])]
-df_good_bad = df[(df['contains_good'])]
-
-# Crear gráfico de polaridad vs longitud del tweet, diferenciando "good" y "bad"
-plt.figure(figsize=(10, 6))
-
-# Graficar tweets con "good"
-plt.scatter(df_good_bad[df_good_bad['contains_good']]['tweet_length'],
-            df_good_bad[df_good_bad['contains_good']]['polarity'],
-            color='green', label='cyberpunk', alpha=0.5)
-
-# Graficar tweets con "bad"
-#plt.scatter(df_good_bad[df_good_bad['contains_bad']]['tweet_length'],
-            #df_good_bad[df_good_bad['contains_bad']]['polarity'],
-            #color='red', label='character', alpha=0.5)
-
-# Personalización del gráfico
-plt.xlabel('Longitud del Tweet')
-plt.ylabel('Polaridad')
-plt.title('Relación entre Polaridad y Longitud del Tweet para "Good" y "Bad"')
-plt.legend()
-plt.grid(True)
-plt.savefig("grafico_polaridad.png")
+#descomentar dependiendo de que imagenes quieras ver
+#grafico_polaridad_1p('cyberpunk', df)
+#grafico_polaridad_2p('good', 'bad', df)
+#grafico_polaridad_2p('steam', 'playstation', df)
+#grafico_polaridad_2p('patch','bugs',df)
+#grafico_polaridad_2p('graphics','performance',df)
+#grafico_polaridad_2p('story', 'gameplay', df)
