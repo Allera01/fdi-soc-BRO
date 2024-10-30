c8760100bf33a6ae2503fad438276d8af630224a
Allera01 <alvaro@DESKTOP-IA7EM4F>
2024-10-30 12:56:20 +0100

    Se sacan los 10 bi-gramas y tri-gramas más repetidos

--- practica_2/main.py
+++ practica_2/main.py
@@ -4,9 +4,12 @@ import nltk
 import re
 from nltk.tokenize import word_tokenize
 from textblob import TextBlob
+from nltk import ngrams
+from collections import Counter
 import string
 import matplotlib.pyplot as plt
 
+""" 
 tknzr = nltk.tokenize.TweetTokenizer()
 
 nltk.download('punkt_tab')
@@ -15,17 +18,17 @@ nltk.download('punkt_tab')
 def preprocess(text):
     tokens = [t.lower() for t in tknzr.tokenize(text)]
     return tokens
-
+ """
 #Quitar las URL y los signos de puntuación
 def remove_punctuation(text):
     url_pattern = r'http[s]?://\S+|www\.\S+'
     # Eliminar URLs del texto
     text_without_urls = re.sub(url_pattern, '', text)
-    
     # Tokenizar el texto
     tokens = word_tokenize(text_without_urls)
     # Filtrar tokens para eliminar signos de puntuación
     tokens = [token.lower() for token in tokens if token not in string.punctuation]
+    #tokens = [word for word in words if word.lower() not in stop_words]
     return tokens
 
 def grafico_polaridad_2p(palabra1, palabra2, datos):
@@ -106,6 +109,21 @@ with open('token_counts.txt', 'w') as f:
     f.write(token_counts.to_string())
 """
 #print(token_counts) 
+
+bigramas = []
+trigramas = []
+
+for tokens in df['tokens']:
+    bigramas.extend(ngrams(tokens, 2))  # Generar bigramas
+    trigramas.extend(ngrams(tokens, 3))  # Generar trigramas
+
+# Contar frecuencias de bigramas y trigramas
+bigramas_frecuentes = Counter(bigramas)
+trigramas_frecuentes = Counter(trigramas)
+
+# Mostrar los 10 bigramas y trigramas más comunes
+print("Bigramas más comunes:", bigramas_frecuentes.most_common(10))
+print("Trigramas más comunes:", trigramas_frecuentes.most_common(10))
 
 #descomentar dependiendo de que imagenes quieras ver
 #grafico_polaridad_1p('cyberpunk', df)
