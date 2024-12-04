6aaac03a7b50d18e8041d7855b8effcb60ba252b
Usuario Local <margal15@ucm.es>
2024-12-04 15:39:09 +0100

    Main.py pyppeteer saca comentarios

--- proyecto_final/main.py
+++ proyecto_final/main.py
@@ -1,17 +1,41 @@
-import descarga
-import anlalisis
-import click
+import asyncio
+from pyppeteer import launch
 
-@click.command(
-    help="\n"
-)  # Define el comando principal
-@click.argument("file", type=click.File("r"))
-@click.option(
-    "-a", "--all", is_flag=True, help="Se ejecutan todas las funciones.\n"
-)
-# Define argumentos que el usuario debe pasar (en este caso, el archivo de lista de aristas)
-def my_main(file, all):
-    print("hola")
+async def obtener_comentarios(video_url):
+    # Lanza el navegador
+    browser = await launch(args=['--no-sandbox'])  # headless=True para no abrir la ventana del navegador
+    page = await browser.newPage()
+    
+    # Ve al video de YouTube
+    await page.goto(video_url)
+    
+    # Espera a que los comentarios se carguen (puede que tengas que ajustar el selector)
+    await page.waitForSelector('ytd-comments')
 
-if __name__ == "__main__":
-    my_main()
+    # Desplazarse hacia abajo para cargar más comentarios (opcional)
+    for _ in range(10):  # Se desplaza 3 veces hacia abajo
+        await page.evaluate('window.scrollBy(0, 1000)')
+        await asyncio.sleep(2)
+
+    # Extrae los comentarios
+    comentarios = await page.evaluate('''() => {
+        let comentarios = [];
+        let commentElements = document.querySelectorAll('ytd-comment-thread-renderer #content-text');
+        commentElements.forEach(comment => {
+            comentarios.push(comment.innerText);
+        });
+        return comentarios;
+    }''')
+
+    # Cierra el navegador
+    await browser.close()
+
+    for i, comentario in enumerate(comentarios[:50]):
+        print(f"{i+1}: {comentario}")
+
+# Ejecuta la función
+video_url = 'https://www.youtube.com/watch?v=QqLGF_ghc8A'
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+comentarios = loop.run_until_complete(obtener_comentarios(video_url))
+
